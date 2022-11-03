4cabd706bfb778fda2b1485d32be94a1b53f3190
Elsayed Hassan <eh600039@gmail.com>
2022-11-03 15:10:15 +0200

    add scroll to the button

--- README.md
+++ README.md
@@ -6,8 +6,37 @@
 
 ## Instructions
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+The starter project has some HTML and CSS styling to display a static version of the Landing Page project.
+You'll need to convert this project from a static project to an interactive one.
+This will require modifying the HTML and CSS files, but primarily the JavaScript file.
 
 To get started, open `js/app.js` and start building out the app's functionality
 
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+For specific, detailed instructions, 
+look at the project instructions in the Udacity Classroom.
+
+## Installation
+
+Extract zip file and open index.html in google chrom or any browser 
+
+## Usage
+
+a multi-section landing page, with a dynamically updating navigational menu based on the amount of content that is added to the page.
+
+## Devlopment 
+Change fonte 
+Add 5 Sections in html file and modfiy thier id ,class , content
+In app.js file 
+Define Global Variables section and navBar.
+Create functoin to addsection to navBar and use loop(for..of).
+use an event while scroll to add class by use classList (your-active-class) and using getBoundingClientRect().top.
+Making  Clicking on a navigation item should scroll to the appropriate section of the page by addeventlistner
+Use scrollintoView(smooth).
+create button in html file .
+Define variable to scroll Top button.
+When the user scrolls down from the top of the document, show the button Using window.scroll
+add conditional to window.scroll function (window.scrollY > window.innerHeight) and chang css style by js;
+addeventlistner click and create function When the user clicks on the button, scroll to the top of the document
+style scroll top button with css 
+style navbar with css 
+style footer with css 
--- js/app.js
+++ js/app.js
@@ -13,20 +13,20 @@ function createMenuItems() {
 }
 createMenuItems();
 
-// use an event (window.onscroll) while scroll to add class (your-active-class) and using getBoundingClientRect().top.
-window.onscroll = function () {
-    document.querySelectorAll("section").forEach(function (active) {
-        if (active.getBoundingClientRect().top >= -400 &&
-            active.getBoundingClientRect().top <= 150) {
-            active.classList.add("your-active-class");
-        }
-        else {
-            active.classList.remove("your-active-class");
-        }
+// use an event while scroll to add class (your-active-class) and using getBoundingClientRect().top.
+window.addEventListener("scroll", function () {
+        document.querySelectorAll("section").forEach(function (active) {
+            if (active.getBoundingClientRect().top >= -400 &&
+                active.getBoundingClientRect().top <= 150) {
+                active.classList.add("your-active-class");
+            }
+            else {
+                active.classList.remove("your-active-class");
+            }
+        });
     });
-};
 
-// Clicking on a navigation item should scroll to the appropriate section of the page
+//Making  Clicking on a navigation item should scroll to the appropriate section of the page
 //Use scrollintoView(smooth).
 navBar.addEventListener("click", (e) => {
     e.preventDefault();
@@ -34,3 +34,23 @@ navBar.addEventListener("click", (e) => {
         document.getElementById(`${e.target.dataset.nav}`).scrollIntoView({ behavior: "smooth" });
     }
 });
+
+// Define variable to scroll Top button
+const scrollBtn = document.querySelector("#scrollBtn");
+
+// When the user scrolls down from the top of the document, show the button
+window.onscroll = function () {
+    if (window.scrollY > window.innerHeight) {
+        scrollBtn.style.display = "block";
+    } else {
+        scrollBtn.style.display = "none";
+    }
+};
+
+// When the user clicks on the button, scroll to the top of the document
+scrollBtn.addEventListener("click", function topFunction() {
+   /* document.body.scrollTop = 0;*/
+    document.documentElement.scrollTop = 0;
+    scrollIntoView({ behavior: "smooth" });
+})
+
